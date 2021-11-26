20cd241161b3fab0fcffac520095ef1b99711a56
Eknath Raut <eknathraut09@gmail.com>
2021-11-26 19:31:01 +0530

    push

---
+++ day3.js
@@ -0,0 +1,12 @@
+// let elem=document.getElementById('click');
+// console.log(elem)
+
+// let elemclass=document.getElementsByClassName('container');
+// console.log(elemclass)
+// elemclass[1].style.background="yellow";
+
+tn=document.getElementsByTagName('div');
+console.log(tn)
+createElement=document.createElement('p');
+createElement.innertext="this is a para";
+tn[0].appendchild(createElement);
